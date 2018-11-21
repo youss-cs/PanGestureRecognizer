f86821b7b9c60eb5af4cc0b65f34daef8dd29541
YouSS <youss.cs@gmail.com>
2018-11-21 14:04:55 +0100

    Initial Commit

---
+++ PanGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PanGesture
+//
+//  Created by YouSS on 11/21/18.
+//  Copyright © 2018 YouSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
